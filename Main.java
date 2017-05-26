18e22d8ae08ba735f047202fa459e73a72057c45
TitanHacker <timl8@hotmail.com>
2017-05-26 10:32:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,16 +24,22 @@ public class Main extends JavaPlugin implements Listener{
 	public Main plugin;
 	public List<String> spells = new ArrayList<String>();
 	public getTargets  getTargets = new getTargets();
+	public Spark Spark = new Spark(this);
 	public PotionWave PotionWave = new PotionWave(this);
 	public Confuse confuse = new Confuse(this);
+	public ExplosionWave ExplosionWave = new ExplosionWave(this);
+	public Storm Storm = new Storm(this);
 	
 	public void onEnable() {
 		plugin = this;
 		PluginDescriptionFile pdffile = getDescription();
 		Logger logger = Logger.getLogger("Minecraft");
 		logger.info(pdffile.getName() + pdffile.getVersion() + "Was enabled.");	
+		spells.add("Spark");
 		spells.add("PotionWave");
 		spells.add("Confuse");
+		spells.add("ExplosionWave");
+		spells.add("Storm");
 		
 		//you need to register the event
 		getServer().getPluginManager().registerEvents(this, this);
@@ -44,27 +50,27 @@ public class Main extends JavaPlugin implements Listener{
 		Logger logger = Logger.getLogger("Minecraft");
 		logger.info(pdffile.getName() + pdffile.getVersion() + "Was Disabled.");
 	}
-	
+	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
-		if(label.equalsIgnoreCase("ew")) {
+		if(label.equalsIgnoreCase("uw")) {
 			if(!(sender instanceof Player)) {
 				sender.sendMessage(ChatColor.RED + "You have to be ingame to use this command");
 			}else{
-				if(sender.hasPermission("empirewand.use")) {
+				if(sender.hasPermission("empirewand.uw")) {
 					Player p = (Player)sender;
 					ItemStack stack = new ItemStack(Material.BLAZE_ROD);
 					ItemMeta stackMeta = stack.getItemMeta();
 					stackMeta.setDisplayName(ChatColor.RED + "Empire" + ChatColor.AQUA + " Wand");
 					stack.setItemMeta(stackMeta);
-					ChatUtilities.sendMessage(p, "You received the EmpireWand");
+					ChatUtilities.sendMessage(p, ChatColor.AQUA + "You received the EmpireWand");
 					p.getInventory().addItem(stack);
 				}else{
-					sender.sendMessage(ChatColor.RED + "You dont have permisions");
+					sender.sendMessage(ChatColor.RED + "You dont have permisions!");
 				}
 			}
 		}
-		return false;
+		return true;
 	}
 	
 	@EventHandler
@@ -72,29 +78,34 @@ public class Main extends JavaPlugin implements Listener{
 	 if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK ) {
 		Player p = e.getPlayer();
 		ItemStack stack = p.getItemInHand();
-		if(stack != null &&stack.getType() == Material.BLAZE_ROD && stack.hasItemMeta() && stack.getItemMeta().getDisplayName().equals(ChatColor.RED + "Empire" + ChatColor.AQUA + " Wand"));
+		if(stack != null &&stack.getType() == Material.BLAZE_ROD && stack.hasItemMeta() && stack.getItemMeta().getDisplayName().equals(ChatColor.RED + "Empire" + ChatColor.AQUA + " Wand")){
 		int SpellSelected = stack.getDurability();
-		if(SpellSelected < 1) {
-			stack.setDurability((short)(SpellSelected + 1));
+		if(SpellSelected < 4) {
+			stack.setDurability((short) (SpellSelected + 1));
 			p.getWorld().playEffect(p.getLocation(), Effect.STEP_SOUND, 119, 30);
 		}else {
 			stack.setDurability((short) 0);
 		}
-		ChatUtilities.sendMessage(p, ChatColor.AQUA + spells.get(SpellSelected) + ChatColor.GREEN + " sellected");
-		
-	 }if(e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
+		ChatUtilities.sendMessage(p, ChatColor.AQUA + spells.get(SpellSelected) + ChatColor.GREEN + " selected");
+			}
+	 	}
+	 if(e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
 		  Player p = e.getPlayer();
 		  ItemStack stack = p.getItemInHand();
-		  if(stack != null &&stack.getType() == Material.BLAZE_ROD && stack.hasItemMeta() && stack.getItemMeta().getDisplayName().equals(ChatColor.RED + "Empire" + ChatColor.AQUA + " Wand"));
+		  if(stack != null &&stack.getType() == Material.BLAZE_ROD && stack.hasItemMeta() && stack.getItemMeta().getDisplayName().equals(ChatColor.RED + "Empire" + ChatColor.AQUA + " Wand")){
 		  int SpellSelected = stack.getDurability();
-		  if(SpellSelected == 0)
-		  this.PotionWave.onCast(p);
-	 }else if(SpellSelected == 1){
-		 this.Confuse.onCast(p);
+		  if(SpellSelected == 1) {
+		  this.Spark.onCast(p);
+	 }else if(SpellSelected == 2){
+		 this.PotionWave.onCast(p);
+	 }else if(SpellSelected == 3){
+		 this.confuse.castSpell(p);
+	 }else if(SpellSelected == 4){
+		 this.ExplosionWave.onCast(p);
+	 }else if(SpellSelected == 0){
+		 this.Storm.onCast(p);
 	 }
-
+		  }
+	 }
 	}
-
-
-
 }
